025362a5c487349181d7730350e920387101a017
HR-SU <1286804583@qq.com>
2021-01-30 10:56:07 +0800

    feat: use json to sink

--- common/src/main/java/flink/sink/MessageSerialize.java
+++ common/src/main/java/flink/sink/MessageSerialize.java
@@ -1,12 +1,20 @@
 package flink.sink;
 
 import flink.types.Output;
+import flink.utils.BandwidthDetection;
 import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.json.JSONObject;
 
 public class MessageSerialize implements SerializationSchema<Output> {
     @Override
     public byte[] serialize(Output element) {
-        String results = element.serialize();
-        return String.format("%-16s", results).getBytes();
+        JSONObject jsonObject = new JSONObject();
+        double avgBand = BandwidthDetection.getAvg();
+        if(avgBand > 0) {
+            jsonObject.put("bandwidth", avgBand);
+        }
+        jsonObject.put("message", element.serialize());
+        String jsonString = jsonObject.toString();
+        return String.format("%-16d%s", jsonString.length(), jsonString).getBytes();
     }
 }
