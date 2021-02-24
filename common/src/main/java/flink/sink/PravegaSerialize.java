2b9576c211862f5d252010becbd0c365f8be41b8
HR-SU <1286804583@qq.com>
2021-02-24 19:39:29 +0800

    feat(PravegaSerialize.java): store event time into Pravega

--- common/src/main/java/flink/sink/PravegaSerialize.java
+++ common/src/main/java/flink/sink/PravegaSerialize.java
@@ -3,9 +3,15 @@ package flink.sink;
 import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 
+import java.nio.ByteBuffer;
+
 public class PravegaSerialize implements SerializationSchema<Tuple2<Long, byte[]>> {
     @Override
     public byte[] serialize(Tuple2<Long, byte[]> element) {
-        return element.f1;
+        Long eventTime = element.f0;
+        ByteBuffer byteBuffer = ByteBuffer.allocate(8 + element.f1.length);
+        byteBuffer.putLong(eventTime);
+        byteBuffer.put(element.f1);
+        return byteBuffer.array();
     }
 }
